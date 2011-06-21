0bce7c3a42ad38d97d5173e74bca36918c252738
Raphael Kallensee <raphael@kallensee.name>
2011-06-21 21:02:27 +0200

    Applied minor changes from re_publica version, bumped up to version 0.2.6

--- contents/app/assistants/schedule-assistant.js
+++ contents/app/assistants/schedule-assistant.js
@@ -659,3 +659,16 @@ ScheduleAssistant.prototype.parseDate = function(xCalDate){
         'minute'    : xCalDate.substr(11,2)
     }
 }
+
+ScheduleAssistant.prototype.strip_tags = function(input, allowed) {
+    // FUNCTION FOUND HERE:
+    // http://phpjs.org/functions/strip_tags:535
+    // (GPL / MIT licenses)
+    allowed = (((allowed || "") + "").toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join(''); // making sure the allowed arg is a string contai
+    var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi,
+        commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
+    return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
+        return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
+    });
+}
+
